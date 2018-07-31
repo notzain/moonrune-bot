f8981472c9853ca93db9ea0f8e751f176c668ab1
Zain Ahmad <zain.x.ahmad@gmail.com>
2018-07-31 18:26:17 +0200

    Initial commit

---
+++ src/commands/google.js
@@ -0,0 +1,17 @@
+const google = require('google');
+
+module.exports = (message, args) => {
+  const query = args.join(' ');
+  google(query, (error, result) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const validLink = result.links.find(link => link.href !== null);
+    if (validLink) {
+      message.channel.send(validLink.href);
+    }
+  });
+}
+
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const Discord       = require('discord.js');
+const config        = require('../config/config.json');
+const handleCommand = require('./commands.js')
+
+const client = new Discord.Client({disableEveryone: true});
+
+const sadEmotes = [
+  'IchigoCry',
+  'ChitogeCry',
+  'KonataCry',
+  'illyacry',
+  'TheSaddest',
+  'kuropatsad',
+]
+
+client.on('ready', async () => {
+  console.log(client.user.username);
+});
+
+client.on('message', async message => {
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (!handleCommand(command, message, args)) {
+    const emoteToFind = sadEmotes[Math.floor(Math.random() * sadEmotes.length)];
+    const emote = client.emojis.find('name', emoteToFind);
+
+    if (emote) {
+      message.channel.send(`I don't recognize command '${command}' ${emote}`);
+    }
+  }
+});
+
+client.login(config.token);
