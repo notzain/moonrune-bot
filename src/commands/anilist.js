1e18c3c08c2c4b3576f0ffdc0802072d41147361
Zain Ahmad <zain.x.ahmad@gmail.com>
2018-07-31 21:13:44 +0200

    Fix anime. Hidden dependency fix (probably should do a pull request...)

--- src/commands/anilist.js
+++ src/commands/anilist.js
@@ -2,24 +2,35 @@ const ani        = require('anifetch');
 const config     = require('../../config/config.json');
 const Discord    = require('discord.js');
 
+const providers = ['kitsu', 'mal', 'myanimelist', 'anilist'];
+
 module.exports = (message, args) => {
-  ani.search('anilist', 'anime', args.join(' '))
+  let provider = 'anilist';
+  let query = args.slice();
+
+  if (providers.includes(args[0])) {
+    provider = args[0];
+    query = args.slice(1);
+  }
+
+  query = query.join(' ');
+  ani.search(provider, 'anime', query)
     .then(res => {
       const anime = res[0];
 
-      const embed = new Discord.RichEmbed()
+      const embed =  new Discord.RichEmbed()
         .setAuthor(anime.title_canonical)
-        .setTitle(anime.title_romaji)
         .setDescription(anime.synopsis)
         .setImage(anime.cover)
         .setURL(anime.url)
         .addField('Episodes', anime.episodes)
-        .addField('Status', anime.status)
         .addField('Rating', anime.rating)
 
       message.channel.send(embed);
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      message.channel.send(`Can't find anime: '${query}'. FeelsBadMan`);
+    })
 }
 
 
