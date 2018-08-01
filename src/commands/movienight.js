51ded5c12295ba9fe786b82d595dcc20e6f0990e
Zain Ahmad <zain.x.ahmad@gmail.com>
2018-08-01 20:15:03 +0200

    Added movie night (anilist) list option

--- src/commands/movienight.js
+++ src/commands/movienight.js
@@ -1,13 +1,120 @@
-const usage = `usage: movienight [command] [...args] \n
-              list [ default = all, seen, unseen ] \n
-              add  [ movie ] \n
-              del  [ movie ] \n
-              finished [ movie ] \n
-              `
+const Discord = require('discord.js');
+const JsonDB   = require('node-json-db');
+const fetch = require('node-fetch');
+
+const config = require('../../config/config.json');
+
+var query = `
+query ($user: String) {
+  MediaListCollection(userName: $user, type: ANIME) {
+    lists {
+      name,
+      entries {
+        media {
+          title {
+            romaji
+          }
+        },
+        score,
+        completedAt {
+          year,
+          month,
+          day
+        }
+      }
+    }
+  }
+}
+`;
+// Define our query variables and values that will be used in the query request
+const variables = {
+  user: "AviansMovies"
+};
+// Define the config we'll need for our Api request
+const url = 'https://graphql.anilist.co',
+    options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+
+const sortByDate = (entries) => entries.sort((a, b) => {
+  return new Date(a.date) - new Date(b.date);
+})
+
+const sortByDateReversed = (entries) => sortByDate(entries).reversed()
+
+const commandList = (message, args) => {
+  console.log(args);
+  fetch(url, options)
+    .then(response => {
+      response.json().then(json => {
+        if (!response.ok) return;
+
+        const listToFind = json.data.MediaListCollection.lists.find(entry =>
+          entry.name.toLowerCase() == args[0].toLowerCase()
+        );
+
+        const iterableMap = listToFind
+          ? [listToFind]
+          : json.data.MediaListCollection.lists;
+
+        const data = iterableMap.map(list => {
+          const entries = list.entries.map(entry => {
+            const title = entry.media.title.romaji;
+            const score = entry.score;
+
+            const completed = entry.completedAt;
+            const date = completed.year !== null
+              ? new Date(completed.year, completed.month, completed.day)
+              : null;
+
+            return {
+              title: title,
+              score: score,
+              date: date
+            }
+          })
+
+          return {
+            name: list.name,
+            entries: entries
+          }
+        });
+
+        data.forEach(list => {
+          const format = list.entries.map(entry => {
+            const score = entry.score != 0 ? `, ${entry.score}/10` : '';
+            const date = entry.date ? ` (${entry.date.toDateString()})` : '';
+
+            return `${entry.title}${score}${date}`;
+          })
+
+          const embed = new Discord.RichEmbed()
+            .setAuthor(list.name)
+            .setDescription(format.join('\n'));
+
+          message.channel.send(embed);
+        });
+      })
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
 
 module.exports = (message, args) => {
-  if (args == 0) {
-    message.channel.send(usage);
+  switch (args[0]) {
+    case 'list': {
+      commandList(message, args.slice(1));
+    }
   }
 }
 
