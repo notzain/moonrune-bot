ee58da43f8b3eef3d9a17c4adb6970fa699db8a3
Zain Ahmad <zain.x.ahmad@gmail.com>
2018-08-04 00:32:02 +0200

    Seperate movienight subcommands to seperate files

--- src/commands/movienight.js
+++ src/commands/movienight.js
@@ -1,129 +1,15 @@
-const Discord = require('discord.js');
-const fetch   = require('node-fetch');
-
-const query = `
-query ($user: String) {
-  MediaListCollection(userName: $user, type: ANIME) {
-    lists {
-      name,
-      entries {
-        media {
-          title {
-            romaji
-          }
-        },
-        score,
-        completedAt {
-          year,
-          month,
-          day
-        }
-      }
-    }
-  }
-}
-`;
-// Define our query variables and values that will be used in the query request
-const variables = {
-  user: "AviansMovies"
-};
-// Define the config we'll need for our Api request
-const url = 'https://graphql.anilist.co',
-    options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            query: query,
-            variables: variables
-        })
-    };
-
-const sortByDate = (entries) => entries.sort((a, b) => {
-  return new Date(a.date) - new Date(b.date);
-})
-
-const sortByTitle = (entries) => entries.sort((a, b) => {
-  return a.title.localeCompare(b.title);
-})
-
-
-const commandList = (message, args) => {
-  fetch(url, options)
-    .then(response => {
-      response.json().then(json => {
-        if (!response.ok) return;
-
-        const listToFind = json.data.MediaListCollection.lists.filter(entry =>
-          args.find(arg => arg.toLowerCase() === entry.name.toLowerCase())
-        );
-
-        const iterableList = listToFind.length > 0
-          ? listToFind
-          : json.data.MediaListCollection.lists;
-
-        const data = iterableList.map(list => {
-          const entries = list.entries.map(entry => {
-            const title = entry.media.title.romaji;
-            const score = entry.score;
-
-            const completed = entry.completedAt;
-            const date = completed.year !== null
-              ? new Date(completed.year, completed.month, completed.day)
-              : null;
-
-            return {
-              title: title,
-              score: score,
-              date: date
-            }
-          })
-
-          return {
-            name: list.name,
-            entries: entries
-          }
-        });
-
-        data.forEach(list => {
-          let entries = list.entries;
-
-          if (args.includes('--sort=date')) {
-            sortByDate(entries);
-          } else if (args.includes('--sort=title')) {
-            sortByTitle(entries);
-          }
-
-          if (args.includes('--reversed')) {
-            entries = entries.reverse();
-          }
-
-          const format = entries.map(entry => {
-            const score = entry.score != 0 ? `, ${entry.score}/10` : '';
-            const date = entry.date ? ` (${entry.date.toDateString()})` : '';
-
-            return `${entry.title}${score}${date}`;
-          })
-
-          const embed = new Discord.RichEmbed()
-            .setAuthor(list.name)
-            .setDescription(format.join('\n'));
-
-          message.channel.send(embed);
-        });
-      })
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
+const commandList    = require('./movienight/list');
+const commandHistory = require('./movienight/history');
 
 module.exports = (message, args) => {
   switch (args[0]) {
     case 'list': {
       commandList(message, args.slice(1));
+      break;
+    }
+    case 'history': {
+      commandHistory(message, args);
+      break;
     }
   }
 }
---
+++ src/commands/movienight/history.js
@@ -0,0 +1,45 @@
+const fetch   = require('node-fetch');
+const Discord     = require('discord.js');
+
+const query = `
+query ($user: String) {
+  User(name: $user) {
+    about
+  }
+}
+`;
+// Define our query variables and values that will be used in the query request
+const variables = {
+  user: "AviansMovies"
+};
+// Define the config we'll need for our Api request
+const url = 'https://graphql.anilist.co',
+    options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+
+const commandHistory = (message, args) => {
+  fetch(url, options)
+    .then(response => {
+      response.json().then(json => {
+        if (!response.ok) return;
+
+        message.channel.sendCode('javascript', json.data.User.about);
+      })
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+module.exports = (message, args) => {
+  commandHistory(message, args);
+}
